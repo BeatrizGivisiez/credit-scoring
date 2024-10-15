1c3e8b2d1aff22cc6c152557b63d297e9de191b4
Beatriz Givisiez <beatriz.givisiez@hotmail.com>
2024-10-15 21:09:37 +0100

    feat: :sparkles: excluido options constants

--- src/constants/options.tsx
+++
@@ -1,6 +0,0 @@
-import { TABLE_GROUPS } from "@/app/_mocks/datatableeg";
-
-export const options = TABLE_GROUPS.map((group) => ({
-  value: `${group.parentClient} - ${group.nif}`,
-  label: `${group.parentClient} - ${group.nif}`
-}));
