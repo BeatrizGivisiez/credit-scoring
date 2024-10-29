fb55055a90b9404947a98f31cf719a687f2e089a
Beatriz Givisiez <beatriz.givisiez@hotmail.com>
2024-10-29 16:49:36 +0000

    feat: :sparkles: /gre - fix rota

--- src/app/api/notInGroupEntity/get.ts
+++ src/app/api/notInGroupEntity/get.ts
@@ -7,7 +7,7 @@ export async function GET(): Promise<NextResponse> {
     return NextResponse.json({ error: "API_URL não está definida" }, { status: 500 });
   }
 
-  const url: string = `https://credit-score-dbgfaceagwhphsb2.canadacentral-01.azurewebsites.net/api/Entity/GetAllNotInGroup`; // Adicione o endpoint correto aqui
+  const url: string = `${apiUrl}Entity/GetAllNotInGroup`; // Adicione o endpoint correto aqui
   const headers: HeadersInit = {
     accept: "application/ld+json",
     "Cache-Control": "no-cache"
@@ -24,7 +24,7 @@ export async function GET(): Promise<NextResponse> {
     }
 
     const data: EntityNotInGroupDTO[] = await response.json();
-    // console.log("Dados recebidos NotInGroupEntity ==>:", data); // Log para verificar a resposta
+    console.log("Dados recebidos NotInGroupEntity ==>:", data); // Log para verificar a resposta
 
     return NextResponse.json(data); // Retorna os dados em formato JSON
   } catch (error: any) {
